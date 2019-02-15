d75e2d6a04f944874d836e74184c61522aed6a68
Oscar <Oscarg_1991@yahoo.com>
2019-02-15 15:41:29 -0700

    css/database/postallcharinfo/seeds

--- schema.sql
+++ schema.sql
@@ -3,6 +3,3 @@ DROP DATABASE IF EXISTS newsfeed;
 -- Creates the "blogger" database --
 CREATE DATABASE newsfeed;
 
-DROP DATABASE IF EXISTS characterAdd;
-
-CREATE DATABASE characterAdd;
---
+++ seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO Characters (id, name, abilities, weapons, origin, appearance, affiliations, strength, health, createdAt, updatedAt)
+VALUES ('1', "The Flash", "Super Speed", "Speed Force", "Central City", "FLASH COMICS #1 (1940)", "Justice League", 75, 75, '2019-02-14 21:53:22', '2019-02-14 21:53:22');
+
+INSERT INTO Characters (id, name, abilities, weapons, origin, appearance, affiliations, strength, health, createdAt, updatedAt)
+VALUES ('2', "Batman", "Martial Artist/Combat Strategy", "Advanced Technology", "Gotham City", "DETECTIVE COMICS #27 (1939)", "Justice League", 70, 70, '2019-02-14 21:53:22', '2019-02-14 21:53:22');
+
+INSERT INTO Characters (id, name, abilities, weapons, origin, appearance, affiliations, strength, health, createdAt, updatedAt)
+VALUES ('3', "Thor", "Superhuman Strength", "Mjolnir Hammer", "Asgard", "JOURNEY INTO MYSTERY #83 (1962)", "Avengers", 80, 80, '2019-02-14 21:53:22', '2019-02-14 21:53:22');
+
+INSERT INTO Characters (id, name, abilities, weapons, origin, appearance, affiliations, strength, health, createdAt, updatedAt)
+VALUES ('4', "Iron Man", "Genius with Powered Armor Suit", "Repulsor Rays", "Long Island, NY", "TALES OF SUSPENSE #39 (1963)", "Avengers", 75, 65, '2019-02-14 21:53:22', '2019-02-14 21:53:22');
